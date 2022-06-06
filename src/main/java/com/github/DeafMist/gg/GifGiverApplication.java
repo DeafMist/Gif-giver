0cd194dde075f744596e279401324b5cff56d97c
Protsenko Roman <romapr70@gmail.com>
2022-06-06 18:07:26 +0300

    Add SpringBoot to the project

---
+++ src/main/java/com/github/DeafMist/gg/GifGiverApplication.java
@@ -0,0 +1,13 @@
+package com.github.DeafMist.gg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifGiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifGiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/DeafMist/gg/GifGiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.DeafMist.gg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifGiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
